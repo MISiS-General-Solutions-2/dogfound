a406bcffe1a08be240a384a5f65e567404ddf0a5
vlasov <aavlasov2000@gmail.com>
2021-10-24 18:15:07 +0300

    add CORS

--- core/api/routing.go
+++ core/api/routing.go
@@ -1,11 +1,18 @@
 package api
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Serve() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		MaxAge:       12 * time.Hour,
+	}))
 
 	api := router.Group("/api")
 	api.POST("/image/by-classes", getImagesByFeatures)
--- core/go.mod
+++ core/go.mod
@@ -3,6 +3,7 @@ module dogfound
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.8
