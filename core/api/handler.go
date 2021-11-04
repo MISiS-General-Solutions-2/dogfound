bfb77121c03d56d24ae9f8d46338b4f534dd541c
vlasov <aavlasov2000@gmail.com>
2021-11-04 23:28:57 +0300

    make crop visualization optional

--- core/api/handler.go
+++ core/api/handler.go
@@ -5,23 +5,30 @@ import (
 	"dogfound/database"
 	"image/color"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func getImage(ctx *gin.Context) {
 	name := ctx.Param("name")
+	omit, _ := strconv.Atoi(ctx.Query("omit_crop"))
 	a, err := database.GetAdditionalInfo(name)
 	if err != nil {
 		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	imgWithCrop, err := cv.DrawRect(database.GetImagePath(name), a.Crop, color.RGBA{0, 255, 0, 128}, 2)
-	if err != nil {
-		ctx.AbortWithError(http.StatusInternalServerError, err)
-		return
+	image := database.GetImagePath(name)
+	if omit != 1 {
+		imgWithCrop, err := cv.DrawRect(image, a.Crop, color.RGBA{0, 255, 0, 128}, 2)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.Data(http.StatusOK, "image/jpeg", imgWithCrop)
+	} else {
+		ctx.File(image)
 	}
-	ctx.Data(http.StatusOK, "image/jpeg", imgWithCrop)
 }
 
 func getImagesByFeatures(ctx *gin.Context) {
